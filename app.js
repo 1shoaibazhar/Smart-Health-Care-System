26b22fb6f3f5fdb3f4f39ab56d866869996f33d7
Shoaib <74708002+1shoaibazhar@users.noreply.github.com>
2022-04-21 16:39:06 +0500

    Added manage account option

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -20,6 +21,8 @@ mongoose.connect("mongodb://localhost:27017/SmartHealthCareSystem");
 // Setting view engine as ejs
 app.set("view engine", "ejs");
 
+// app.use(morgan('dev'));
+
 // EXPRESS SPECIFIC STUFF
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -32,7 +35,6 @@ app.get("/", (req, res) => {
       console.log("Collection droped");
     };
   });
- 
   var admin_data = {
     UserName: "admin",
     Password: "1234",
@@ -40,13 +42,12 @@ app.get("/", (req, res) => {
   var myData = new admin(admin_data);
   myData.save(function(err,admin){
     if (err) return console.error(err);
-    console.log("Admin added succesfully");
   })
 });
 
 
 app.get("/login", (req, res) => {
-  res.render("login", { title: "Login" });
+  res.render("login", { title: "Login" , unavailable : ""});
 });
 
 app.get("/patient_signup", (req, res) => {
@@ -86,29 +87,242 @@ app.post("/patient_signup", (req, res) => {
     });
 });
 
-app.post("/login", (req, res) => {
+
+app.get("/update_patient/:id", (req,res)=>{
+  const useriD = req.params.id;
+  console.log(useriD);
+  patient.findById(useriD)
+    .then(result => {
+      res.render("update_info_patient", {User: result});
+    })
+})
+
+// Deleting a patient
+app.get("/delete_patient/:id", (req,res)=>{
+  const useriD = req.params.id;
+  console.log(useriD);
+  patient.findByIdAndDelete(useriD)
+    .then(result => {
+      // res.json({ redirect: '/login'});
+      res.redirect('/');
+    })
+    
+    .catch(err=> console.log(err));
+})
+
+
+// For updating patient data
+app.post("/update_info_patient", (req,res)=>{
+  console.log(req.body);
+  let patient_username = req.body.UserName;
+  
+  patient.findOne({UserName : patient_username}, function (err, doc){
+    if (doc){
+      let Name = "";
+      let Email = "";
+      let Gender = "";
+      let CNIC = '';
+      let PhoneNumber = "";
+      let Password = "";
+      let Address = "";
+      
+      if (req.body.Name == ""){
+        Name = doc.Name;
+      }
+      else{
+        Name = req.body.Name;
+      }
+
+      if (req.body.Email == ""){
+        Email = doc.Email;
+      }
+      else{
+        Email = req.body.Email;
+      }
+      
+      if (req.body.Gender == ""){
+        Gender = doc.Gender;
+      }
+      else{
+        Gender = req.body.Gender;
+      }
+
+      if (req.body.CNIC == ''){
+        CNIC = doc.CNIC;
+      }
+      else{
+        CNIC = req.body.CNIC;
+      }
+
+      if (req.body.PhoneNumber == ""){
+      PhoneNumber = doc.PhoneNumber;
+      }
+      else{
+      PhoneNumber = req.body.PhoneNumber;
+      }
+
+      if (req.body.Password == ""){
+      Password = doc.Password;
+      }
+      else{
+      Password = req.body.Password;
+      }
+
+      if (req.body.Address == ""){
+      Address = doc.Address;
+      }
+      else{
+      Address = req.body.Address;
+      }
+
+      UserName = patient_username;
+      let new_patient = {UserName, Name, Email, Gender, CNIC, PhoneNumber, Password, Address};
+
+      patient.findOneAndUpdate({UserName : patient_username}, new_patient, {upsert: true}, function(err, doc1) {
+        if (err) return res.send(500, {error: err});
+        var title_display = "DashBoard | " + patient_username;
+        res.render("patient_dashboard", {title: title_display, User : patient_username, userID: doc1._id, UserObj: doc1});
+      });
+
+    }
+  });
+
+
+})
+
+
+
+app.get("/update_employee/:id", (req,res)=>{
+  const useriD = req.params.id;
+  console.log(useriD);
+  employee.findById(useriD)
+    .then(result => {
+      res.render("update_info_employee", {User: result});
+    })
+})
+
+// Deleting an Employee
+app.get("/delete_employee/:id", (req,res)=>{
+  const useriD = req.params.id;
+  console.log(useriD);
+  employee.findByIdAndDelete(useriD)
+    .then(result => {
+      res.redirect('/');
+    })
+    
+    .catch(err=> console.log(err));
+})
+
+
+
+// For updating employee data
+app.post("/update_info_employee", (req,res)=>{
+  console.log(req.body);
+  let employee_username = req.body.UserName;
+  
+  employee.findOne({UserName : employee_username}, function (err, doc){
+    if (doc){
+      let Name = "";
+      let Email = "";
+      let Gender = "";
+      let EmployeeNumber = '';
+      let PhoneNumber = "";
+      let Password = "";
+      let Address = "";
+      
+      if (req.body.Name == ""){
+        Name = doc.Name;
+      }
+      else{
+        Name = req.body.Name;
+      }
+
+      if (req.body.Email == ""){
+        Email = doc.Email;
+      }
+      else{
+        Email = req.body.Email;
+      }
+      
+      if (req.body.Gender == ""){
+        Gender = doc.Gender;
+      }
+      else{
+        Gender = req.body.Gender;
+      }
+
+      if (req.body.EmployeeNumber == ''){
+        EmployeeNumber = doc.EmployeeNumber;
+      }
+      else{
+        EmployeeNumber = req.body.EmployeeNumber;
+      }
+
+      if (req.body.PhoneNumber == ""){
+        PhoneNumber = doc.PhoneNumber;
+      }
+      else{
+        PhoneNumber = req.body.PhoneNumber;
+      }
+
+      if (req.body.Password == ""){
+        Password = doc.Password;
+      }
+      else{
+       Password = req.body.Password;
+      }
+
+      if (req.body.Address == ""){
+       Address = doc.Address;
+      }
+      else{
+        Address = req.body.Address;
+      }
+
+      UserName = employee_username;
+      let employee_patient = {UserName, Name, Email, Gender, EmployeeNumber, PhoneNumber, Password, Address};
+
+      employee.findOneAndUpdate({UserName : employee_username}, employee_patient, {upsert: true}, function(err, doc1) {
+        if (err) return res.send(500, {error: err});
+        var title_display = "DashBoard | " + employee_username;
+        res.render("employee_dashboard", {title: title_display, User : employee_username, userID: doc1._id, UserObj: doc1});
+      });
+
+    }
+  });
+
+
+})
+
+
+
+app.post("/login", async(req, res) => {
   const UserName = req.body.UserName;
   const Password = req.body.Password;
 
   var title_display = "DashBoard | " + UserName;
 
-  employee.findOne({UserName : UserName, Password : Password}, function (err, doc){
+  await employee.findOne({UserName : UserName, Password : Password}, function (err, doc){
     if (doc){
-      res.render("patient_dashboard", {title: title_display, User : UserName});
+      res.render("employee_dashboard", {title: title_display, User : UserName, userID: doc._id, UserObj: doc});
     }
-  });
+  }).clone().catch(function(err){ console.log(err)});
 
-  patient.findOne({UserName : UserName, Password : Password}, function (err, doc){
+  await patient.findOne({UserName : UserName, Password : Password}, function (err, doc){
     if (doc){
-      res.render("patient_dashboard", {title: title_display, User : UserName});
+      res.render("patient_dashboard", {title: title_display, User : UserName, userID: doc._id, UserObj: doc});
     }
-  });
+  }).clone().catch(function(err){ console.log(err)});
   
-  admin.findOne({UserName : UserName, Password : Password}, function (err, doc){
+  await admin.findOne({UserName : UserName, Password : Password}, function (err, doc){
     if (doc){
-      res.render("patient_dashboard", {title: title_display, User : UserName});
+      res.render("patient_dashboard", {title: title_display, User : UserName, userID: doc._id, UserObj: doc});
     }
-  });
+  }).clone().catch(function(err){ console.log(err)});
+
+
+  res.render("login", { title: "Login" , unavailable : "Invalid Username/Password"});
+
 
 });
 
