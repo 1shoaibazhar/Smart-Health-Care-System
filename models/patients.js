f6b36f60964105de4bfce57e23a8192c682f2b29
Anna <annaahmedghumman@gmail.com>
2022-05-04 19:52:38 +0500

    Files are updated with input validation checks that are implemented using express-validator. GUI files updated with value and bootstrap links

--- models/patients.js
+++ models/patients.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 
 // Define Mongoose Schema
 const patientSchema = new mongoose.Schema({
-      Name: String,
-      Email: String,
-      UserName: {
-      type: String,
-      unique : true
-      },
-      Gender: String,
-      CNIC: Number,
-      PhoneNumber : String,
-      Password: String,
-      Address: String
+	Name: String,
+	Email: String,
+	UserName: {
+		type: String,
+		unique: true,
+	},
+	Gender: String,
+	CNIC: Number,
+	PhoneNumber: Number,
+	Password: String,
+	Address: String,
 });
 
 // Model
 const patient = mongoose.model("Patient", patientSchema);
-module.exports = patient
+module.exports = patient;
